47ecacd3f08a56c0199949d4ebf0f23f237bffaa
Sachini <sachiniwijayasekar@gmail.com>
2026-03-28 02:40:32 +0530

    Delete Harm Test Files

--- tmp_test/test_db.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-const dt = require('date-fns');
-
-// Add to your current env later
-const supabaseUrl = process.env.VITE_SUPABASE_URL; // We will extract this
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY;
-
-console.log(supabaseUrl, supabaseAnonKey);
