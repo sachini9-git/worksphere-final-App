30da985228087bca04382c22dccd9618d0137034
Sachini <sachiniwijayasekar@gmail.com>
2026-04-18 01:50:45 +0530

    vite change

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      define: {
-        'import.meta.env.VITE_GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY)
-      },
+
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
